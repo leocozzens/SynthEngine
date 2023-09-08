e928cde8b84743605a04cf6390be06a0b3608974
Leo Cozzens <leo@narthexnetworks.com>
2023-09-07 22:59:37 -0500

    Reformat

--- src/main.c
+++ src/main.c
@@ -1,23 +1,39 @@
+#include <stdio.h>
+
+// General
+#define WAV_OUT "test.wav"
+
 // Riff chunk
 #define CHUNK_ID "RIFF"
 #define FORMAT "WAVE"
 
 // FMT sub-chunk
-#define SUBCHUNK1_ID "fmt " 
-#define SUBCHUNK1_SIZE 16
-#define AUDIO_FORMAT 1
-#define NUM_CHANNELS 2
-#define SAMPLE_RATE 44100
-#define BYTE_RATE (SAMPLE_RATE * NUM_CHANNELS * (SUBCHUNK1_SIZE / 8))
-#define BLOCK_ALIGN (NUM_CHANNELS * (SUBCHUNK1_SIZE / 8))
-#define BITS_PER_SAMPLE 16
+#define SUBCHUNK1_ID "fmt "
 
 // Data sub-chunk
 #define SUBCHUNK2_ID "data"
 
 int main(void) {
-    char chunkSize[4];
-    char subChunk2Size[4];
+    char chunkSize[4] = "----";
+
+    int subchunk1Size = 16;
+    int audioFormat = 1;
+    int numChannels = 2;
+    int sampleRate = 44100;
+    int byteRate = sampleRate * numChannels * (subchunk1Size / 8);
+    int blockAlign = numChannels * (subchunk1Size / 8);
+    int bitsPerSample = 16;
+
+    char subChunk2Size[4] = "----";
+
+    FILE *wav = fopen(WAV_OUT, "w");
+    if(wav == NULL) {
+        perror("fopen");
+        return 1;
+    }
+
+    fwrite(&CHUNK_ID, 1, strlen(CHUNK_ID), wav);
+    fwrite(&chunkSize, 1, sizeof(chunkSize), wav);
 
     return 0;
 }
